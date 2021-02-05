093faf8816953571498d810356219be97b628731
Nayra Deniz <nayradb@gmail.com>
2021-02-05 16:05:51 +0100

    version 13 de odoo

---
+++ __manifest__.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Developments Manager',
+    'version': '1.0',
+    'author': 'EEP - Profesor',
+    'description': """ Modulo gestion de solicitudes de desarrollo.""",
+    'depends': ['base'],
+    'application': True,
+    'data' : [
+        'views/devman_views.xml',
+            ],
+}
